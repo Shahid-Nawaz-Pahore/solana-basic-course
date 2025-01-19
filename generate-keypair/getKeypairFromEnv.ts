80e8065969387e83956f1847d5bfe7ab3fbb713f
Shahid Nawaz Blockchain Engineer <shahidnawaz.edu.pk@gmail.com>
2025-01-19 15:40:04 +0500

    complete lesson 1 in which learn about generate keypair and load keypair from env

---
+++ generate-keypair/getKeypairFromEnv.ts
@@ -0,0 +1,11 @@
+// import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log(
+  `✅ Finished! We've loaded our secret key securely, using an env file!`
+);
